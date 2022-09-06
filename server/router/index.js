21a5739f5116a5d3de473b26eed95b3fb8f127ac
Mai Mohammed <maymohammedabuabed@gmail.com>
2022-09-06 16:35:27 +0300

    connect sign up and login buttons to /login /signup endpoints

--- server/router/index.js
+++ server/router/index.js
@@ -5,9 +5,17 @@ const { addUser } = require('../controllers');
 // const createError = require('http-errors');
 
 router.get('/', (req, res) => {
-  res.status(200).sendFile(join('..', '..', 'public', 'index.html'));
+  res.status(200).sendFile(join(__dirname, '..', '..', 'public', 'index.html'));
 });
 
 router.post('/signup', addUser);
+
+router.get('/signup', (req, res) => {
+  res.status(200).sendFile(join(__dirname, '..', '..', 'public', 'signup.html'));
+});
+
+router.get('/login', (req, res) => {
+  res.status(200).sendFile(join(__dirname, '..', '..', 'public', 'login.html'));
+});
 
 module.exports = router;
