3755d69da61eaf9477b0410e6e9c5d0ad81c9441
Mai Mohammed <maymohammedabuabed@gmail.com>
2022-09-07 20:56:17 +0300

    write verify token middlewares + handle token through pages

--- server/router/index.js
+++ server/router/index.js
@@ -1,12 +1,21 @@
 const { join } = require('path');
 const router = require('express').Router();
-
 const { addUser, loginUser } = require('../controllers');
 const { serverError } = require('../errors');
 const privateRouter = require('./private');
-// const createError = require('http-errors');
+const verifyToken = require('../middlewares/verifyToken');
 
-router.use('/private', privateRouter );
+router.use((req, res, next) => {
+  res.clearCookie('token', { path: '/login' });
+  next();
+});
+
+router.get('/logout', (req, res) => {
+  res.clearCookie('token').redirect('/');
+});
+
+router.use('/private', verifyToken, privateRouter);
+
 router.get('/', (req, res) => {
   res.status(200).sendFile(join(__dirname, '..', '..', 'public', 'index.html'));
 });
@@ -14,14 +23,13 @@ router.get('/', (req, res) => {
 router.post('/signup', addUser);
 
 router.get('/signup', (req, res) => {
-  res.status(200).sendFile(join(__dirname, '..', '..', 'public', 'signup.html'));
+  res.clearCookie('token').status(200).sendFile(join(__dirname, '..', '..', 'public', 'signup.html'));
 });
-
 
 router.post('/login', loginUser);
 
 router.get('/login', (req, res) => {
-  res.status(200).sendFile(join(__dirname, '..', '..', 'public', 'login.html'));
+  res.clearCookie('token').status(200).sendFile(join(__dirname, '..', '..', 'public', 'login.html'));
 });
 
 router.use(serverError);
